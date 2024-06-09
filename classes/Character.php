79d958e3e1adfd232365c5795b2ec5b07dcda74c
fufuYang <imfuting@gmail.com>
2024-06-09 12:39:31 +0800

    feat : 建立共同屬性

---
+++ classes/Character.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PlayGame;
+
+trait Character
+{
+    public $database;
+    public $healthPoints = 100;
+    public $physicalAttack;
+    public $magicAttack;
+    public $physicalDefense;
+    public $magicDefense;
+    public $magicPoints;
+    public $luckiness;
+    
+    public function character()
+    {
+        $this->database;
+        $this->healthPoints;
+        $this->physicalAttack;
+        $this->magicAttack;
+        $this->physicalDefense;
+        $this->physicalAttack;
+        $this->magicDefense;
+        $this->magicPoints;
+        $this->luckiness;
+    }
+    
+}
