c78abb68a4436d7039fb94ca9eb7162e21f48d12
fufuYang <imfuting@gmail.com>
2024-06-13 00:12:40 +0800

    feat: 修改主選單功能
    - 修改"創建角色"為呼叫 Player 類別的相應方法，以提高程式碼結構和重用性
    - 新增呼叫"開始遊戲"功能，以啟動遊戲戰鬥
    - 新增查看"遊戲紀錄"功能，以便玩家查看自己的遊戲進度和成就
    - 更新主選單的選項和對應的功能，提供更完整的遊戲體驗

--- classes/View.php
+++ classes/View.php
@@ -3,6 +3,7 @@
 namespace PlayGame;
 
 require_once 'Player.php'; // 創建角色
+require_once 'Fight.php'; // 創建角色
 
 class View
 {
@@ -13,31 +14,24 @@ class View
 
     public static function mainScreen()
     {
-        $Player = new Player();
-
         echo "(1).創建角色\n(2).開始遊戲\n(3).遊戲紀錄\n(4).結束遊戲\n";
         $selectAction = readline('請選擇:');
         if ($selectAction == '1') {
             // 創建角色
-            $player = new Player();
-            echo"\n";
-            $playerName = readline("請輸入玩家姓名：");
-            echo"\n";
-            $player->selectRole($playerName)->creatPoint();
-            $player->saveToDatabase($playerName);
-
-            echo "玩家角色已成功保存到資料庫！\n";
+            $player = new Player(); // 實例化 Player 物件
+            self::returnToMainMenu();
         } elseif ($selectAction == '2') {
             // 開始遊戲
-
-
+            $fight = new Fight();
+            self::returnToMainMenu();
         } elseif ($selectAction == '3') {
-            // 遊戲紀錄
-
-
+            // 遊戲紀錄                       
+            $gameRecord = new GameRecord();
+            $gameRecord->getRecord();
+            self::returnToMainMenu();
         } elseif ($selectAction == '4') {
             // 結束遊戲
-            echo '遊戲結束\n';
+            echo '遊戲結束';
             exit();
         } else {
             echo "輸入無效，請重新選擇(1/2/3/4)" . "\n";
@@ -57,6 +51,3 @@ class View
         echo "\033[2J\033[0;0H";
     }
 }
-
-$View = new View();
-View::mainScreen();
