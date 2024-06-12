f1b6af82e24863b593266e66ac4706a04db8cca2
fufuYang <imfuting@gmail.com>
2024-06-13 00:13:34 +0800

    feat: 新增遊戲主程式入口
    - 新增 index.php 檔案作為遊戲的主程式入口
    - 在 index.php 中創建了 index 類別，並在構造函數中實例化 View 類別，並調用其 mainScreen 方法
    - 這個修改使得遊戲的入口更清晰，並確保了遊戲開始時顯示主選單

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-echo"text";
+
+namespace PlayGame;
+
+require_once 'classes//View.php'; // 創建角色
+class index
+{
+    public function __construct()
+    {
+        $View = new View();
+        View::mainScreen();
+    }
+}
+$index = new index();
